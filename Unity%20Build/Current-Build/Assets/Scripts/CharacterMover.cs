a6255db8b346f9827bd89bab11739545e21c5aa5
limejuice123 <elliotpage9@gmail.com>
2016-11-05 13:13:48 +0000

    Add files via upload

---
+++ Unity%20Build/Current-Build/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMover : MonoBehaviour {
+
+    public Transform mover;
+
+	void Start ()
+    {
+        this.gameObject.SetActive(false);
+        mover.position = new Vector3(10, -1, 0);
+	}
+
+    void CharacterEnterStore()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    void CharacterLeaveStore()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    void Update ()
+    {
+	    while (isActiveAndEnabled == true)
+        {
+            mover.Translate(-2, 0, 0);
+        }
+	}
+}
